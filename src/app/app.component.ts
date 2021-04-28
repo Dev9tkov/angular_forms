38c92de55d4a5de05780eb137cc17dc996bfd600
Dev9tkov <idev9tkov@gmail.com>
2021-04-28 09:59:52 +0500

    4. Валидаторы 5. Визуализация состояний

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
   form: FormGroup
@@ -12,14 +12,16 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.form = new FormGroup({
       /** принимает на вход начальное значение и список валидаторов */
-      email: new FormControl(''),
-      password: new FormControl(null)
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
     })
   }
 
   submit() {
-    console.log('Form:', this.form);
-    const formData = {...this.form.value};
-    console.log('FormData: ', formData)
+    if (this.form.valid) {
+      console.log('Form:', this.form);
+      const formData = {...this.form.value};
+      console.log('FormData: ', formData)
+    }
   }
 }
